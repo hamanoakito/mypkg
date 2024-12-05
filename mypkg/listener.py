b7ab37846179007f540cbb459f377aebaa067536
hamanoakito <tsukigamichibikuisekaidochu@gmail.com>
2024-12-05 22:34:11 +0900

    change listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,6 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 
+class Listener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.pub = self.create_subscription(Int16, "countup", self.cb, 10)
+    def cb(self, msg):
+        self.get_logger().info("Listen: %d" % msg.data)
+
+def main():
+    rclpy.init()
+    node = Listener()
+    rclpy.spin(node)
+
+"""
 rclpy.init()
 node = Node("listener")
 
@@ -14,3 +27,4 @@ def cb (msg):
 def main():
     pub = node.create_subscription(Int16, "countup", cb, 10)
     rclpy.spin(node)
+"""
